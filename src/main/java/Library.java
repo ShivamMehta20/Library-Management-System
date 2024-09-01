35123a4ef1244c008b71379de207fc9d0f76f9fc
ShivamMehta20 <shivamm9784942@gmail.com>
2024-09-01 15:40:26 +0530

    Implement borrow book feature

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -23,6 +23,29 @@ public class Library {
         books.add(book);
     }
 
+    // function for borrow Book
+    public void borrowBook(String isbn) {
+
+        Book book = findBook(isbn);
+        // checking if book is not available then throw exception
+        if (!book.isAvailable()) {
+            throw new IllegalArgumentException("Book with " + book.getIsbn() + "is not available");
+        }
+        
+        book.setAvailable(false);
+    
+    }
+
+    // function will return book with isbn equals to argument isbn
+    private Book findBook(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
     // function will return list of Book object which is available
     public List<Book> getAvailableBooks() {
         List<Book> availableBooks = new ArrayList<>();
