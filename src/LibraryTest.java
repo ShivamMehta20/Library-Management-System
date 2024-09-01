9e85a7dfdffd0bd73b4caffd2305271d6ab911cc
ShivamMehta20 <shivamm9784942@gmail.com>
2024-09-01 15:31:16 +0530

    add test for borrowing book

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -16,12 +16,22 @@ public class LibraryTest {
         samplebook = new Book("123456", "sample book", "me", 2009);
     }
 
-    // testing addBook feature
+    // testing that book is added or not
     @Test
     public void testAddBook() {
         library.addBook(samplebook);
         // asserts that book is added
         assertEquals(1, library.getAvailableBooks().size());
 
+    }
+// testing Book is borrowed or not  
+    @Test
+    public void testBorrowBook()
+    {
+
+        library.addBook(samplebook);
+        library.borrowBook("123456");
+        // if all available book is 0 then book is borrowed
+        assertEquals(0, library.getAvailableBooks().size());
     }
 }
